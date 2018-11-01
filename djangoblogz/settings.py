17df2c99039e924fa69ac135b06cb6610e433cce
Kapil Varunshri <kapil.varunshri@citrix.com>
2018-11-01 07:30:39 -0400

    added base templates

--- djangoblogz/settings.py
+++ djangoblogz/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djangoblogz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
