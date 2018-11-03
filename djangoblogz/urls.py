9431597485ff8c79651ed361c1f6b041b3ffbc51
Kapil Varunshri <kapil.varunshri@citrix.com>
2018-11-03 19:29:18 -0400

    added models and migrated

--- djangoblogz/urls.py
+++ djangoblogz/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
     path('about/', views.about),
     path('', views.homepage),
 ]
