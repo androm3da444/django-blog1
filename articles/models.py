291b802629101e7a9de5765b36cef4b30dfdf99f
Kapil Varunshri <kapil.varunshri@citrix.com>
2018-11-04 09:04:11 -0500

    updated templates

--- articles/models.py
+++ articles/models.py
@@ -11,3 +11,9 @@ class Article(models.Model):
 
     # python manage.py makemigrations
     # python manage.py migrate
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:50] + '...'
