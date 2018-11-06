8e44fca2ff4e06ec5a4b20a77b7c9684bd4a2bc2
Kapil Varunshri <kapil.varunshri@citrix.com>
2018-11-06 17:31:27 -0500

    added accounts app and setup url path

--- djangoblogz/urls.py
+++ djangoblogz/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
     path('about/', views.about),
     path('', views.homepage),
