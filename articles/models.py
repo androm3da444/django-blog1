99913ac4a3334e2d1b69d71ab908414f1ea2380a
Kapil Varunshri <kapil.varunshri@citrix.com>
2018-11-08 17:35:08 -0500

    completed styling and remaining functionality

--- articles/models.py
+++ articles/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
-    # add in author later
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     # python manage.py makemigrations
     # python manage.py migrate
--- djangoblogz/urls.py
+++ djangoblogz/urls.py
@@ -4,13 +4,14 @@ from .import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
     path('about/', views.about),
-    path('', views.homepage),
+    path('', article_views.article_list, name="home"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
