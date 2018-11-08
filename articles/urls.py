6f062ca6a25e8dc9ae6157a256c9bf4ff5a176b3
Kapil Varunshri <kapil.varunshri@citrix.com>
2018-11-08 15:37:37 -0500

    new forms added, created login/logout routes

--- articles/urls.py
+++ articles/urls.py
@@ -5,6 +5,7 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.article_list, name="list"),
+    path('create/', views.article_create, name="create"),
     #re_path('(?P<slug>[\w-]+)/$', views.article_detail),
     path('<slug>/', views.article_detail, name="detail"),
 ]
